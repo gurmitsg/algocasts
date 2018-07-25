21878c7528df55c0825f5479230b7ed44d3af7a8
gurmitsg <gurmitsg@gmail.com>
2018-07-25 18:01:53 +0800

    25jul

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+reverse = (str) => {
+    return str.split('').reduce((acc,cur) => cur + acc,'')
+}
+
+reverse1 = (str) => {
+    return str.split('').reverse().join('')
+}
+
+reverse2 = (str) => {
+    let newStr = ''
+    for (s of str) {
+        newStr = s + newStr
+        debugger
+    }
+    return newStr
+}
+
+reverse2('123456')
 
 module.exports = reverse;
+
+
